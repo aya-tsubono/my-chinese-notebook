d2db83ce25c18c64cd21858aa2b02b09463d6f7a
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-03 14:36:29 +0900

    修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordは英字混合でないと登録できない' do
+      it 'passwordは英字と数字が混合でないと登録できない' do
         @user.password = 'ababab'
         @user.password_confirmation = @user.password
         @user.valid?
