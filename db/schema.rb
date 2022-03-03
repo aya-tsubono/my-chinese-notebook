6395bc990a3b9c2fed0f77a851c5ca9e3a9a2355
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-03 23:59:13 +0900

    wordテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_071046) do
+ActiveRecord::Schema.define(version: 2022_03_03_144339) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2022_02_28_071046) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "words", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "words", null: false
+    t.string "words_meaning", null: false
+    t.string "words_pronunciation", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
