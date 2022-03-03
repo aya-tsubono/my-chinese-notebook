afba45bd9c1062cc1dc1877105c4c70c4df8d67f
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-04 00:06:19 +0900

    バリデーションの記述

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,7 @@
 class Word < ApplicationRecord
   belongs_to :user
+
+  validates :words, presence: true
+  validates :words_meaning, presence: true
+  validates :words_pronunciation, presence: true
 end
