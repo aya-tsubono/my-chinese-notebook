fa67c004d0bf7b2aff88a7704e8cd98605be2fa8
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-04 00:04:16 +0900

    アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :words
 
   validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
   validates :registration_date, presence: true
