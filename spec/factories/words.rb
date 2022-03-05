11424799be57ac34c13eb9be7ab739fb910c3d97
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-05 13:29:50 +0900

    factories botの記述

--- spec/factories/words.rb
+++ spec/factories/words.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :word do
-    
+    words { Faker::Lorem.sentence }
+    words_meaning { Faker::Lorem.sentence }
+    words_pronunciation { Faker::Lorem.sentence }
   end
 end
