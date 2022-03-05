dc56137b10a6ccfc699de5e8a727ddf73d77dc1e
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-05 13:04:41 +0900

    createアクションの定義

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -5,4 +5,14 @@ class WordsController < ApplicationController
   def new
     @word = Word.new
   end
+
+  def create
+    @word = Word.new(word_params)
+    if @word.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
 end
