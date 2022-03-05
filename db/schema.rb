bde5610959a7289e67b53ed2b966df7e02baf7cc
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-05 17:29:48 +0900

    テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_144339) do
+ActiveRecord::Schema.define(version: 2022_03_05_082422) do
+
+  create_table "sentences", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "sentences", null: false
+    t.string "sentences_meaning", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sentences_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -33,4 +42,5 @@ ActiveRecord::Schema.define(version: 2022_03_03_144339) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "sentences", "users"
 end
