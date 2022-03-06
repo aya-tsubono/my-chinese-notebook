c8303faea1d58b0c978c54e3cda2fe2891e663c3
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-06 22:27:55 +0900

    wordのアソシエーションの追記

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,6 @@
 class Word < ApplicationRecord
+  belongs_to :user
+
   validates :words, presence: true
   validates :words_meaning, presence: true
   validates :words_pronunciation, presence: true
