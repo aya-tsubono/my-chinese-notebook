9330eb9132001c1bab8a4878080709753fc7ba71
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-06 21:51:35 +0900

    テストの記述

--- spec/models/sentence_spec.rb
+++ spec/models/sentence_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Sentence, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '例文の登録' do
+    context '登録できる場合' do
+      it '全ての項目を正しく入力すれば登録できる' do
+        expect(@sentence).to be_valid
+      end
+    end
+    context '登録できない場合' do
+      it 'sentencesが空では作成できない' do
+        @sentence.sentences = ''
+        @sentence.valid?
+        expect(@sentence.errors.full_messages).to include("Sentences can't be blank")
+      end
+      it 'sentences_meaningが空では作成できない' do
+        @sentence.sentences_meaning = ''
+        @sentence.valid?
+        expect(@sentence.errors.full_messages).to include("Sentences meaning can't be blank")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @sentence.user = nil
+        @sentence.valid?
+        expect(@sentence.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
