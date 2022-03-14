9207dc3a391771cf12a67e6535aade02a3f5e763
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-14 12:14:09 +0900

    トップページの配置変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root to: 'words#index'
+  root to: 'users#index'
   
   resources :users, only: [:show]
   resources :words, only: [:index, :new, :create, :edit, :update]
