32030903a735e010c7a36174082a767ae9bc517a
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-15 18:14:33 +0900

    userコントローラーにアクションの追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @days = Date.today - @user.registration_date
     @word = @user.words
+    @sentence = @user.sentences
   end
 end
