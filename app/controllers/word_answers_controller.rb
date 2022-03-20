fc69bec842f132d37163577edf40581e3e12b790
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-20 20:03:06 +0900

    正解不正解ボタンの仮設置

--- app/controllers/word_answers_controller.rb
+++ app/controllers/word_answers_controller.rb
@@ -1,8 +1,8 @@
 class WordAnswersController < ApplicationController
   def index
     select_word = Word.order("RAND()").limit(1).ids
-    word_id = Word.find(select_word)
-    @mean = word_id.pluck(:words_meaning)
-    @name = word_id.pluck(:words_name)
+    @word_id = Word.find(select_word)
+    @mean = @word_id.pluck(:words_meaning)
+    @name = @word_id.pluck(:words_name)
   end
 end
