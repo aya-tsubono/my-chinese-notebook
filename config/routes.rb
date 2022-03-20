284ba05429099947e84826d81680dced4bde951a
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-20 20:04:19 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   
   resources :users, only: [:show, :index]
   resources :words, only: [:index, :new, :create, :edit, :update]
-  resources :word_answers, only: [:index]
+  resources :word_answers, only: [:index, :new, :create]
   resources :sentences, only: [:index, :new, :create, :edit, :update]
 end
