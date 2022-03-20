cf5da30ea43ad20cde6905ff6f681cbe20345748
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-20 20:06:35 +0900

    newとcreateアクションの定義

--- app/controllers/word_answers_controller.rb
+++ app/controllers/word_answers_controller.rb
@@ -5,4 +5,17 @@ class WordAnswersController < ApplicationController
     @mean = @word_id.pluck(:words_meaning)
     @name = @word_id.pluck(:words_name)
   end
+
+  def new
+    @word_answer = WordAnswers.new
+  end
+
+  def create
+    @word_answer = WordAnswers.new(answer_params)
+    if @word_answer.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
