46a6e623f85eb1c331b17ad51044729b45a59047
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-22 11:26:12 +0900

    バリデーションの記述

--- app/models/word_answer.rb
+++ app/models/word_answer.rb
@@ -2,4 +2,7 @@ class WordAnswer < ApplicationRecord
   belongs_to :user
   belongs_to :word
 
+  validates :w_correct, presence: true, {numericality: true}
+  validates :w_incorrect, presence: true, {numericality: true}
+
 end
