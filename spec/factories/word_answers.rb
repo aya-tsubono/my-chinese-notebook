1b2a369487a953b32c3bb1f243560c9ae04181c9
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-24 15:00:14 +0900

    factorybotの記述

--- spec/factories/word_answers.rb
+++ spec/factories/word_answers.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :word_answer do
-    
+    w_correct { Faker::Number.between(from: 0, to: 1) }
+    w_incorrect { Faker::Number.between(from: 0, to: 1) }
+    word_id { Faker::Number.digit }
+    association :user
+
   end
 end
