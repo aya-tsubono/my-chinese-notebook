de655cbb1668076d9d1948d4c154e714a65fb0d5
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-25 15:22:20 +0900

    コントローラーの修正

--- app/controllers/word_mean_answers_controller.rb
+++ app/controllers/word_mean_answers_controller.rb
@@ -9,12 +9,12 @@ class WordMeanAnswersController < ApplicationController
   end
   
   def new
-    @word_answer = WordAnswer.new
+    @word_m_answer = WordMeanAnswer.new
   end
 
   def create
-    @word_answer = WordAnswer.new(answer_params)
-    if @word_answer.save
+    @word_m_answer = WordMeanAnswer.new(answer_m_params)
+    if @word_m_answer.save
       redirect_to action: 'index'
     else
       redirect_to root_path
@@ -24,6 +24,6 @@ class WordMeanAnswersController < ApplicationController
   private
 
   def answer_params
-    params.require(:word_answer).permit(:w_correct, :w_incorrect, :word_id).merge(user_id: current_user.id)
+    params.require(:word_m_answer).permit(:m_correct, :m_incorrect, :word_id).merge(user_id: current_user.id)
   end
 end
