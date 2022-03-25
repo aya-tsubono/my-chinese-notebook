a73f446197c136712bf940383a47e4a2adf6528c
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-25 11:34:07 +0900

    indexアクションの記述

--- app/controllers/word_mean_answers_controller.rb
+++ app/controllers/word_mean_answers_controller.rb
@@ -1,2 +1,10 @@
 class WordMeanAnswersController < ApplicationController
+
+  def index
+    select_word = Word.order('RAND()').limit(1).ids
+    @word = Word.find(select_word)
+    @mean = @word.pluck(:words_meaning)
+    @name = @word.pluck(:words_name)
+    @word_id = @word.pluck(:id)
+  end
 end
