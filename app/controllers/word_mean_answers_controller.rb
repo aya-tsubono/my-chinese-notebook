35f0c2c6aba0027091f9f95cee38a6dde5920f55
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-03-25 15:19:43 +0900

    createとnewアクションの記述

--- app/controllers/word_mean_answers_controller.rb
+++ app/controllers/word_mean_answers_controller.rb
@@ -7,4 +7,23 @@ class WordMeanAnswersController < ApplicationController
     @name = @word.pluck(:words_name)
     @word_id = @word.pluck(:id)
   end
+  
+  def new
+    @word_answer = WordAnswer.new
+  end
+
+  def create
+    @word_answer = WordAnswer.new(answer_params)
+    if @word_answer.save
+      redirect_to action: 'index'
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:word_answer).permit(:w_correct, :w_incorrect, :word_id).merge(user_id: current_user.id)
+  end
 end
